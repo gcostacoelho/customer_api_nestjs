96983fca05832826fdd09386db4d2b74334a1df2
gcostacoelho <gcostacoelho2003@gmail.com>
2023-03-01 11:00:05 -0300

    create-update-in-redis

--- src/Services/customer.service.ts
+++ src/Services/customer.service.ts
@@ -2,6 +2,7 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import { redis } from 'src/Database/RedisConfig';
 import { Customer } from "src/Model/Customer";
+import axios from 'axios';
 
 @Injectable()
 export class CustomerService {
@@ -38,14 +39,47 @@ export class CustomerService {
         }
     }
 
-    Update(request, response) {
+    async Update(request, response) {
+        let id = request.params.id;
+        let body = request.body;
+        let customerId = await redis.get(id);
 
+        if (customerId == null) {
+            return response.status(HttpStatus.BAD_REQUEST).json("cliente inexistente")
+        }
+
+        const customerParsed = JSON.parse(customerId);
+
+        if (customerParsed.document != body.document) {
+            if (RegExp(/[a-z]|[-.]/gmi).test(body.document)) {
+
+            } else {
+                customerParsed.document = body.document;
+            }
+        }
+        
+        if (customerParsed.name != body.name) {
+            if (body.name == '') {
+                return response.status(HttpStatus.BAD_REQUEST).json("request inválida");
+            } else {
+                customerParsed.name = body.name;
+            }
+        }
+
+        const data = {
+            "document": customerParsed.document,
+            "name": customerParsed.name
+        }
+
+        await redis.set(id, JSON.stringify(data));
+
+        return response.status(HttpStatus.OK).json(customerParsed)
     }
 
     async Search(request, response) {
-        let id = request.params.id
-
+        let id = request.params.id;
         let customer = await redis.get(id);
+
         const customerParsed = JSON.parse(customer)
 
         return response.status(HttpStatus.OK).json(customerParsed);
