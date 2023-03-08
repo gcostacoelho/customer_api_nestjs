ed7bb43bb3de3efcb1059fa1566fb9d90994adcb
gcostacoelho <gcostacoelho2003@gmail.com>
2023-03-08 15:03:52 -0300

    adjusts-redis-host

--- src/Database/RedisConfig.ts
+++ src/Database/RedisConfig.ts
@@ -1,5 +1,9 @@
 import { Redis } from "ioredis";
+import * as dotenv from 'dotenv';
+
+dotenv.config()
 
 export const redis = new Redis({
-    port: 6379
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT) || 6379
 });
